bd20080847e1c09e170e5e119eb1a1bb73edccc6
David Mayou <davidpmayou@gmail.com>
2019-01-26 09:44:15 -0600

    add get code

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Request library
+using System.Net;
+using System.IO;
 
 namespace myApp
 {
@@ -6,7 +13,16 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        string url = @"http://localhost:5000/api/projects";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+        using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
         }
     }
 }
